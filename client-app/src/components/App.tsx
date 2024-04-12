eab1601880f9f046cf5199c49a40d8e51f344c74
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-04-12 23:41:26 +0200

    demo

--- client-app/src/components/App.tsx
+++ client-app/src/components/App.tsx
@@ -2,15 +2,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import { useAtom } from 'jotai';
-import {User, userAtom, userEffect} from '../state/atoms/user';
+import {jwtAtom, decodeAndSetUserWhenJwtAtomChanges, User, userAtom, sayHiWhenUserChanges} from '../state/atoms/user';
+import {Toaster} from "react-hot-toast";
 
 export default function App() {
     const [user, setUser] = useAtom(userAtom);
-    useAtom(userEffect);
+    const [jwt, setJwt] = useAtom(jwtAtom);
+    useAtom(sayHiWhenUserChanges);
+    useAtom(decodeAndSetUserWhenJwtAtomChanges);
     return (
         <Router>
+            <Toaster />
             <>
                 {JSON.stringify(user, null, 2)}
+                {JSON.stringify(jwt, null, 2)}
                 <button
                     onClick={() => {
                         const user: User = {
@@ -22,6 +27,10 @@ export default function App() {
                 >
                     Randomize
                 </button>
+
+                <button onClick={() => {
+                    setJwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJVc2VybmFtZSI6ImJsYWFhaCIsIklkIjoxfQ.1aQtDZb0Vi8tSIt5YGtgEXCtWSh_9asIMLjzFkbwrN2QOGzA4d4kMFo9MtYfTepQ2k5e5PqTGmZt46HmMxKa3A")
+                }} className="btn">Randomize jwt</button>
             </>
         </Router>
     );
--- client-app/src/state/atoms/user.ts
+++ client-app/src/state/atoms/user.ts
@@ -3,6 +3,7 @@ import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 import { atomEffect } from 'jotai-effect'
 import { toast } from 'react-hot-toast';
+import {decodeJwt} from "./decodeJwt.ts";
 
 export interface User {
     username: string;
@@ -11,12 +12,22 @@ export interface User {
 
 export const userAtom = atomWithStorage<User | null>('user', null);
 
-export const userEffect = atomEffect((get, set) => {
+export const sayHiWhenUserChanges = atomEffect((get, set) => {
     const user = get(userAtom);
     console.log(user)
     if (user) {
-        toast('Welcome back ' + user.username);
+        toast.success(`Welcome back ${user.username}`);
     } else {
         console.log('No user');
+    }
+});
+
+export const jwtAtom = atomWithStorage<string>('token', '');
+
+export const decodeAndSetUserWhenJwtAtomChanges = atomEffect((get, set) => {
+    const jwt = get(jwtAtom);
+    console.log(jwt)
+    if (jwt) {
+        set(userAtom, decodeJwt(jwt));
     }
 });
